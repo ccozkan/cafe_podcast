106c5de2a9313341e627f2aaaaeb4d97e956b699
Çağrı Özkan <cagrio.dev@gmail.com>
2020-05-17 19:45:59 +0300

    Add user tests

--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Content, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'bla' do
+    let!(:user) { FactoryBot.create(:user) }
+    let!(:subscription) { FactoryBot.create(:subscription, user_id: user.id) }
+    let!(:content) { FactoryBot.create(:content, user_id: user.id, subscription_id: subscription.id) }
+
+    context '#validations' do
+      it { is_expected.to validate_presence_of(:url) }
+    end
+  end
+
+  describe '' do
+    let!(:user) { FactoryBot.create(:user) }
+    let!(:subscription) { FactoryBot.create(:subscription, user_id: user.id, url: 'https://feed.podbean.com/podcast.fularsizentellik.com/feed.xml') }
+
+    before do
+      User.update_contents(user)
+    end
+
+    it '' do
+      expect(Content.where(user_id: user.id, subscription_id: subscription.id).empty?).to eq(false)
+    end
+  end
+  User.destroy_all
 end
+
