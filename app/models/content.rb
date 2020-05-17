a9ac4683ae9c56e8137544826e99a88572d91772
Çağrı Özkan <cagrio.dev@gmail.com>
2020-05-17 19:44:07 +0300

    Update model relationships

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,4 +1,8 @@
 class Content < ApplicationRecord
-  belongs_to :subscription
-  belongs_to :user
+  belongs_to :subscription, dependent: :destroy
+  belongs_to :user, dependent: :destroy
+
+  validates :url, presence: true
+  validates :entry_id, uniqueness: { scope: :user,
+                                     message: "user has already this content"}
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,8 @@
 class Subscription < ApplicationRecord
   belongs_to :user
+  has_many :contents
+
+  validates :url, presence: true
+
+
 end
