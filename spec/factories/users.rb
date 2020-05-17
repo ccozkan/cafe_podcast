7063c5f87cd5c90c5e2c8fefab4eda40535a8de0
Çağrı Özkan <cagrio.dev@gmail.com>
2020-05-17 19:44:33 +0300

    Add user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:email) { |n| "person#{n}@example.test"}
+    sequence(:username) { |n| "person#{n}" }
+    password {'testpassword'}
   end
 end
