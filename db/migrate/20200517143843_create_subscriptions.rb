655d7e90c344650e7ae2b063d17ad393a1e579fa
Çağrı Özkan <cagrio.dev@gmail.com>
2020-05-17 20:16:13 +0300

    Dirty fix in subscription migration

--- db/migrate/20200517143843_create_subscriptions.rb
+++ db/migrate/20200517143843_create_subscriptions.rb
@@ -2,9 +2,10 @@ class CreateSubscriptions < ActiveRecord::Migration[6.0]
   def change
     create_table :subscriptions do |t|
       t.string :name
+      t.text :description
       t.string :url
       t.string :media_url
-      t.datetime :last_publish
+      t.date :last_publish_date
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
