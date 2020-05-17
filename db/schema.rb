93003106d5a8d2b45d142c851c4e284dc68d1aba
Çağrı Özkan <cagrio.dev@gmail.com>
2020-05-17 17:41:12 +0300

    Add updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_143641) do
+ActiveRecord::Schema.define(version: 2020_05_17_144000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contents", force: :cascade do |t|
+    t.string "url"
+    t.string "title"
+    t.text "summary"
+    t.date "publish_date"
+    t.integer "duration"
+    t.boolean "listened"
+    t.boolean "starred"
+    t.string "entry_id"
+    t.bigint "subscription_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["subscription_id"], name: "index_contents_on_subscription_id"
+    t.index ["user_id"], name: "index_contents_on_user_id"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.string "media_url"
+    t.datetime "last_publish"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_subscriptions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +55,7 @@ ActiveRecord::Schema.define(version: 2020_05_17_143641) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "contents", "subscriptions"
+  add_foreign_key "contents", "users"
+  add_foreign_key "subscriptions", "users"
 end
