b41dedef8d87bfb46a229d0bba0aa6b6d0eac698
Çağrı Özkan <cagrio.dev@gmail.com>
2020-05-17 20:29:23 +0300

    Update factory

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :subscription do
     name { "cool podcast" }
+    description { 'where i talk about cool stuff'}
     url { "https://feed.coolpodcast.test" }
     media_url { "https://coolpodcast.test/thumbnail" }
-    last_publish { Date.today - 1.day }
+    last_publish_date { nil }
     user { nil }
   end
 end
