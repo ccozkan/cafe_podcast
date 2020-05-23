d95203cbb5494350c0df69459eb94bb5b923cf98
Çağrı Özkan <cagrio.dev@gmail.com>
2020-05-23 22:35:12 +0300

    Podcast search is now service object! 👀

--- app/services/podcast_searcher.rb
+++ app/services/podcast_searcher.rb
@@ -1,13 +1,35 @@
 # frozen_string_literal: true
 
 class PodcastSearcher
-  def self.call(query)
-    url = "https://itunes.apple.com/search?term=#{query}&entity=podcast"
+  attr_reader :query
+
+  def initialize(query, user)
+    @query = query
+    @user = user
+  end
+
+  def call
+    receive_results
+    # results = receive_results
+    # TODO:
+    # DO STUFF
+    # maybe some statistics idk
+  end
+
+  private
+
+  def send_request
+    url = "https://itunes.apple.com/search?term=#{@query}&entity=podcast"
     response = HTTParty.get(url,
                             headers: { 'Accept': 'application/json' },
                             format: :json)
     return nil unless response.code == 200
 
+    response
+  end
+
+  def receive_results
+    response = send_request
     results = []
     response['results'].each do |r|
       result = { 'url': r['feedUrl'],
--- spec/services/podcast_searcher_spec.rb
+++ spec/services/podcast_searcher_spec.rb
@@ -1,13 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe PodcastSearcher, type: :service do
+
   describe 'search_podcasts method' do
-    it 'should find some shows' do
-      results = PodcastSearcher.call('joe rogan')
-      expect(results.length).to be > 0
-      results = PodcastSearcher.call('reverberation radio')
-      expect(results.length).to be > 0
-    end
+    let(:user) { FactoryBot.create(:user)}
+
+
+      it 'should find some shows' do
+        p = PodcastSearcher.new('joe rogan', user)
+        results = p.call
+        expect(results.length).to be > 0
+      end
+
+      it 'should fine more shows' do
+        p = PodcastSearcher.new('reverberation radio', user)
+        p.call
+        results = p.call
+        expect(results.length).to be > 0
+      end
   end
 end
-
