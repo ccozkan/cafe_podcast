021c2b5f1e2b64502c241f04f8b8b0e81bf5a2b1
Çağrı Özkan <cagrio.dev@gmail.com>
2020-05-25 04:37:45 +0300

    Add subscription creation tests

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -8,4 +8,21 @@ RSpec.describe Subscription, type: :model do
       it { is_expected.to validate_presence_of(:url) }
     end
   end
+
+  describe 'add subscription' do
+    let!(:user) { FactoryBot.create(:user) }
+    let!(:sub) { FactoryBot.create(:subscription, user_id: user.id, url: 'http://feeds.soundcloud.com/users/soundcloud:users:340197999/sounds.rss') }
+
+    context 'succesfully creates' do
+      it 'subscriptions and its info' do
+        expect(User.last.subscriptions.last.url).to eq(sub.url)
+        expect(user.subscriptions.last.last_publish_date).not_to eq(nil)
+        expect(user.subscriptions.last.media_url).not_to eq(nil)
+        expect(user.subscriptions.last.name).not_to eq(nil)
+      end
+      it 'updates its contents' do
+        expect(user.subscriptions.last.contents).not_to eq(nil)
+      end
+    end
+  end
 end
