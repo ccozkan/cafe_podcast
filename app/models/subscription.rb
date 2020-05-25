ba9c1fed73ca5b1d87ff79b7b45105ce4706dc17
Çağrı Özkan <cagrio.dev@gmail.com>
2020-05-25 04:39:35 +0300

    Update added feed at first run

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,13 @@
 class Subscription < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   has_many :contents
 
   validates :url, presence: true
+  validates :url, uniqueness: { scope: :user,
+                                     message: "user podcast is already subscribed"}
+  after_create :first_time_run
+
+  def first_time_run
+    User.update_subscribed_podcasts(user, url)
+  end
 end
