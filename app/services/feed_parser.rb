b92ae647160110b429d4723900798b037d184140
Çağrı Özkan <cagrio.dev@gmail.com>
2020-05-31 18:17:31 +0300

    Strip tracking parameters from url's

--- app/services/feed_parser.rb
+++ app/services/feed_parser.rb
@@ -2,10 +2,13 @@
 
 class FeedParser
   def self.call(content)
+    # you no track me
+    stripped_url = content.enclosure_url.split('?', 2)[0]
+
     { title: content.title,
       duration: content.enclosure_length.to_i / 983_040 + 1,
       summary: content.itunes_summary,
-      url: content.enclosure_url,
+      url: stripped_url,
       entry_id: content.entry_id,
       keywords: content.itunes_keywords,
       publish_date: content.published }
