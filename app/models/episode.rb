9f9d4c8b67d2d783d7b082fa004b4229d5e2d96d
Çağrı Özkan <cagrio.dev@gmail.com>
2020-06-28 00:56:25 +0300

    Add validations

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -2,5 +2,7 @@ class Episode < ApplicationRecord
   has_many :interactions
   has_many :users, through: :interactions
   belongs_to :podcast
-  validates :url, presence: true
+
+  validates_presence_of :url
+  validates_uniqueness_of :entry_id
 end
