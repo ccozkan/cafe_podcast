0ba31252b85214d0b597f115a3ffff5362e9d1d8
Çağrı Özkan <cagrio.dev@gmail.com>
2020-06-30 01:00:39 +0300

    Add updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_203645) do
+ActiveRecord::Schema.define(version: 2020_06_07_082413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,22 +41,18 @@ ActiveRecord::Schema.define(version: 2020_06_04_203645) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-  create_table "contents", force: :cascade do |t|
+  create_table "episodes", force: :cascade do |t|
     t.string "url"
     t.string "title"
     t.text "summary"
     t.date "publish_date"
     t.integer "duration"
-    t.boolean "listened", default: false
-    t.boolean "starred", default: false
     t.string "entry_id"
-    t.bigint "subscription_id", null: false
-    t.bigint "user_id", null: false
+    t.text "keywords", default: [], array: true
+    t.bigint "podcast_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "keywords", default: [], array: true
-    t.index ["subscription_id"], name: "index_contents_on_subscription_id"
-    t.index ["user_id"], name: "index_contents_on_user_id"
+    t.index ["podcast_id"], name: "index_episodes_on_podcast_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -70,19 +66,40 @@ ActiveRecord::Schema.define(version: 2020_06_04_203645) do
     t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
-  create_table "subscriptions", force: :cascade do |t|
+  create_table "interactions", force: :cascade do |t|
+    t.bigint "episode_id"
+    t.bigint "user_id"
+    t.boolean "is_starred", default: false
+    t.boolean "is_listened", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["episode_id"], name: "index_interactions_on_episode_id"
+    t.index ["user_id"], name: "index_interactions_on_user_id"
+  end
+
+  create_table "podcasts", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.string "url"
     t.string "media_url"
     t.date "last_publish_date"
     t.integer "number_of_episodes"
-    t.bigint "user_id", null: false
+    t.bigint "original_adder_id"
+    t.text "categories", default: [], array: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "categories", default: [], array: true
     t.string "slug"
-    t.index ["slug"], name: "index_subscriptions_on_slug", unique: true
+    t.index ["original_adder_id"], name: "index_podcasts_on_original_adder_id"
+    t.index ["slug"], name: "index_podcasts_on_slug", unique: true
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.bigint "podcast_id"
+    t.bigint "user_id"
+    t.boolean "is_favorite", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["podcast_id"], name: "index_subscriptions_on_podcast_id"
     t.index ["user_id"], name: "index_subscriptions_on_user_id"
   end
 
@@ -92,6 +109,10 @@ ActiveRecord::Schema.define(version: 2020_06_04_203645) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
@@ -99,7 +120,6 @@ ActiveRecord::Schema.define(version: 2020_06_04_203645) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "contents", "subscriptions"
-  add_foreign_key "contents", "users"
-  add_foreign_key "subscriptions", "users"
+  add_foreign_key "episodes", "podcasts"
+  add_foreign_key "podcasts", "users", column: "original_adder_id"
 end
