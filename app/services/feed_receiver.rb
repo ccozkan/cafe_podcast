f65a29fee0537be8b5bff8dfafe7d1c1df7ea7f9
Çağrı Özkan <cagrio.dev@gmail.com>
2020-07-02 00:13:58 +0300

    Add TODO note

--- app/services/feed_receiver.rb
+++ app/services/feed_receiver.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 class FeedReceiver
+  # TODO:
+  # - handle error
+  # - maybe add retry for few times
+  # - add tests
   def self.call(url)
-    begin
-      response = HTTParty.get(url)
-      if response.success?
-        xml = response.body
-        feed = Feedjira.parse(xml, parser: Feedjira::Parser::ITunesRSS)
-        feed.entries.sort_by!(&:published).reverse!
-        feed
-      end
-    rescue
-      'some error'
+    response = HTTParty.get(url)
+    if response.success?
+      xml = response.body
+      feed = Feedjira.parse(xml, parser: Feedjira::Parser::ITunesRSS)
+      feed.entries.sort_by!(&:published).reverse!
+      feed
     end
   end
 end
